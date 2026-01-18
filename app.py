e689e09247390244fb19944dcbaa5f6d57d8f2f8
Aadesh Singh <aadeshsinghad@gmail.com>
2026-01-19 01:09:11 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
-import streamlit as st
 import pickle
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# load data
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+# vectorizer (same config as training)
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    for i in movie_list:
-        print(new_df.iloc[i[0]].title)
-from main import recommend
 
-movies_dict = pickle.load(open("movie_dict.pkl", "rb"))
-movies= pd.DataFrame(movies_dict)
-st.title('Movie Recommendation System')
+    similarity = cosine_similarity(vectors, vectors[movie_index].reshape(1, -1))
+    distances = similarity.flatten()
 
+    movie_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
 
-selected_movie_name = st.selectbox(
-    "How would you like to be contacted?",
-    movies['title'].values,
-)
-
-if st.button("Recommend"):
-    recommend(selected_movie_name)
-    st.write("selected_movie_name")
+    return [movies.iloc[i[0]].title for i in movie_list]
