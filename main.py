c20b855fee82cb9f62d1f5708bf0908c8d1bf081
Aadesh Singh <aadeshsinghad@gmail.com>
2026-01-19 01:05:04 +0530

    Created main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import pickle
+import pandas as pd
+
+# Load processed data
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movie_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    recommendations = []
+    for i in movie_list:
+        recommendations.append(movies.iloc[i[0]].title)
+
+    return recommendations
