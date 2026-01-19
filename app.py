b119f1015e4534b8db3220eb7d1e46b35c9127ad
Aadesh <aadeshsinghad@gmail.com>
2026-01-19 21:23:18 +0530

    fix app.py streamlit output

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 import streamlit as st
 import pickle
 import pandas as pd
+from main import recommend   
 
-def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    for i in movie_list:
-        print(new_df.iloc[i[0]].title)
-from main import recommend
-
+# load movie data
 movies_dict = pickle.load(open("movie_dict.pkl", "rb"))
-movies= pd.DataFrame(movies_dict)
-st.title('Movie Recommendation System')
+movies = pd.DataFrame(movies_dict)
 
+st.title("Movie Recommendation System")
 
 selected_movie_name = st.selectbox(
-    "How would you like to be contacted?",
-    movies['title'].values,
+    "Select a movie",
+    movies['title'].values
 )
 
 if st.button("Recommend"):
-    recommend(selected_movie_name)
-    st.write("selected_movie_name")
+    recommendations = recommend(selected_movie_name)
+
+    for movie in recommendations:
+        st.write("👉", movie)
